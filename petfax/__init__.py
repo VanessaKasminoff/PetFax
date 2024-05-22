632bcd4fc6abc177b47df578d815c637a5a9bd79
Vanessa Kasminoff <sunnset07@gmail.com>
2024-05-22 16:58:49 -0700

    Created a homepage for the root endpoint and refactored the index for the /pets endpoint, using it as the show page

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask 
+from flask import (Flask, render_template) 
+import json
+
+pets = json.load(open('pets.json'))
 
 def create_app(): 
     app = Flask(__name__)
 
     @app.route('/')
     def hello(): 
-        return 'Hello, PetFax!'
+        return render_template('home.html', pets=pets)
 
     from . import pet
     app.register_blueprint(pet.bp)
