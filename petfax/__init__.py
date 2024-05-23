2baf81122976961255d1398f8ccb5151fc912b6a
Vanessa Kasminoff <sunnset07@gmail.com>
2024-05-23 01:49:03 -0700

    Added styling to homepage

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -7,13 +7,14 @@ import json
 
 load_dotenv()
 
-pets = json.load(open('pets.json'))
+pets = json.load(open('pets.json', 'r', encoding='utf-8'))
 
 def create_app(): 
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PG_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     from .models import model
     model.db.init_app(app)
